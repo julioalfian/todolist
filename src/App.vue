74f3b61e24bc398088279f1d259456fa5fbed772
julio <julioixc@gmail.com>
2020-02-16 10:59:14 +0700

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,91 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    </div>-->
+    <v-app id="inspire">
+      <v-navigation-drawer v-model="drawer" app clipped>
+        <v-list dense>
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-action>
+                <v-icon>dashboard</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Home</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+          <router-link to="/about">
+            <v-list-item>
+              <v-list-item-action>
+                <v-icon>settings</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>About</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar app clipped-left>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+          <v-icon>menu</v-icon>
+        </v-app-bar-nav-icon>
+        <v-toolbar-title>Todolist</v-toolbar-title>
+      </v-app-bar>
+
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout align-center justify-center>
+            <router-view />
+          </v-layout>
+        </v-container>
+      </v-content>
+
+      <v-footer app>
+        <span>&copy; 2019</span>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
+<script>
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+export default {
+  name: "",
+  vuetify: new Vuetify({
+    theme: { dark: true }
+  }),
+  icons: {
+    iconfont: "mdi"
+  },
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
+
 <style lang="scss" scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Material+Icons");
+
+body {
+  font-family: "Roboto", sans-serif;
 }
 
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+// #nav {
+//   padding: 30px;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
